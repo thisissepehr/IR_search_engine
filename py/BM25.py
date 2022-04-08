5b8819a3e377ed154f291a0969b9a4df51f33814
patrick-25 <85232111+patrick-25@users.noreply.github.com>
2022-04-08 15:16:35 +0100

    Update BM25.py

--- py/BM25.py
+++ py/BM25.py
@@ -4,6 +4,72 @@ import math
 import numpy as np
 from multiprocessing import Pool, cpu_count
 
+
+
+class BM25:
+    def __init__(self, corpus, tokenizer=None):
+        self.corpus_size = 0
+        self.avgdl = 0
+        self.doc_freqs = []
+        self.idf = {}
+        self.doc_len = []
+        self.tokenizer = tokenizer
+
+        if tokenizer:
+            corpus = self._tokenize_corpus(corpus)
+
+        nd = self._initialize(corpus)
+        self._calc_idf(nd)
+
+    def _initialize(self, corpus):
+        nd = {}  # word -> number of documents with word
+        num_doc = 0
+        for document in corpus:
+            self.doc_len.append(len(document))
+            num_doc += len(document)
+
+            frequencies = {}
+            for word in document:
+                if word not in frequencies:
+                    frequencies[word] = 0
+                frequencies[word] += 1
+            self.doc_freqs.append(frequencies)
+
+            for word, freq in frequencies.items():
+                try:
+                    nd[word]+=1
+                except KeyError:
+                    nd[word] = 1
+
+            self.corpus_size += 1
+
+        self.avgdl = num_doc / self.corpus_size
+        return nd
+
+    def _tokenize_corpus(self, corpus):
+        pool = Pool(cpu_count())
+        tokenized_corpus = pool.map(self.tokenizer, corpus)
+        return tokenized_corpus
+
+    def _calc_idf(self, nd):
+        raise NotImplementedError()
+
+    def get_scores(self, query):
+        raise NotImplementedError()
+
+    def get_batch_scores(self, query, doc_ids):
+        raise NotImplementedError()
+
+    def get_top_n(self, query, documents, n=5):
+
+        assert self.corpus_size == len(documents), "The documents given don't match the index corpus!"
+
+        scores = self.get_scores(query)
+        top_n = np.argsort(scores)[::-1][:n]
+        return [documents[i] for i in top_n]
+
+    
+    
 class BM25Okapi(BM25):
     def __init__(self, corpus, tokenizer=None, k1=1.5, b=0.75, epsilon=0.25):
         self.k1 = k1
@@ -56,4 +122,4 @@ class BM25Okapi(BM25):
                                                (q_freq + self.k1 * (1 - self.b + self.b * doc_len / self.avgdl)))
         return score.tolist()
     
-    # Use get_batch_scores() on the set of docs, then return the top 10 docs (or however many we want) in order by score.
+    # Use get_batch_scores() on the set of pre-indexed docs, then return the top n docs in score-order with get_top_n().
