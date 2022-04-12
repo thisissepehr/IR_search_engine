fe790441715c39c6475a5d78b5ddaa22fde0bcb6
thisissepehr <a.aminian1377@gmail.com>
2022-04-12 12:52:49 +0100

    new views for backend and stuff

--- www/public/js/events.js
+++ www/public/js/events.js
@@ -1,29 +1,56 @@
 
-document.addEventListener("submit", (event)=>{
-    event.preventDefault()
-    const search = document.getElementById("search-content")
-    console.log(search.value);
-})
+// document.addEventListener("submit", (event)=>{
+//     event.preventDefault()
+//     const search = document.getElementById("search-content")
+//     console.log(search.value);
+// })
 
 // setting the date at the end of the page
 const date =new Date()
 document.getElementById("copyright").innerHTML = "Copy Right &copy "+date.getFullYear()
 
-document.getElementById("search").addEventListener("click",()=>{
-    
+
+// getting data by Clicking!
+document.getElementById("search").addEventListener("click",(event)=>{
+    //preventing from refrehing
+    event.preventDefault()
+    const search = document.getElementById("search-content").value
+
+    // calling the server for this search query
+    /////////////////////////////////////////////
+    /////////////////////////////////////////////
+    /////////////////////////////////////////////
+    const data = getData(search)
+    // console.log(search);
+    //adding results
     let container = document.getElementById("results")
-    // document.removeChild(container)
-    // container = document.createElement("div")
-    // container.id = "holder"
-    // container.classList.add("result-container")
-    container.style.visibility = "visible";
+    
+    clear_reasults(container);
+    
+    // populating the result area
     data.forEach((sample)=>{
         create_result_tile(sample, container)
-    })
+    }) 
+    container.style.visibility = "visible";
+    // TODO: just a test remember to remove it
+    // if (search ==="1"){
+
+    //     data.forEach((sample)=>{
+    //         create_result_tile(sample, container)
+    //     })
+    // }else if(search === "2"){
+    //     data2.forEach((sample)=>{
+    //         create_result_tile(sample, container)
+    //     })
+    // }
 
 })
 
-
+let clear_reasults = (container)=>{
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }      
+}
 let create_result_tile = (sample, container)=> {
     
     // create card
@@ -57,6 +84,53 @@ let create_result_tile = (sample, container)=> {
     container.append(inside_div)
 }
 
+let getData = (query)=>{
+    let dataRecieved = []
+    fetch("?query="+query).then((response)=>{
+        response.json().then((data)=>{
+            if (data.error){
+
+            }else{
+                dataRecieved = data
+            }
+        })
+    })
+    console.log(dataRecieved)
+    return dataRecieved;
+
+}
+
+
+
+const data2= [
+    {
+        title:"something 1000",
+        author:["john Doe", "sep"],
+        url:"https://google.com",
+        year: 2022
+    },
+    {
+        title:"something 2000",
+        author:["jane Doe"],
+        url:"https://google.com",
+        year: 2021
+    },
+    {
+        title:"something 3000",
+        author:["sep Doe"],
+        url:"https://google.com",
+        year: 1982
+    },
+    {
+        title:"something 4000",
+        author:["alex Doe"],
+        url:"https://google.com",
+        year: 2022
+    },
+    
+]
+
+
 const data= [
     {
         title:"something 1",
@@ -84,4 +158,3 @@ const data= [
     },
     
 ]
-
